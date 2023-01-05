e0e4e2290a5fbd20fa9a35fb0c47beb69321464f
sholqui13 <sholguinquiroz1@gmail.com>
2023-01-04 20:11:54 -0500

    Nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from "./banner/banner.component";
+import { BannerModule } from './banner/banner.module';
 
 @NgModule({
-  declarations: [	
-    AppComponent,
-      HeaderComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        BannerComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BannerModule
+    ]
 })
 export class AppModule { }
